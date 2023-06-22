36626adf06763296183352ac8d3ce7994f43e2f2
os-mari.kolodzheieva <os-mari.kolodzheieva@rakuten.com>
2023-06-22 21:18:35 +0200

    Initial commit

---
+++ src/com/hillel/Klasswork/lesson1/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.Klasswork.lesson1;
+
+public class Main {
+
+    //psvm
+    public static void main(String[] args) {
+
+        //sout
+        System.out.print("Hello");
+        System.out.println("!");
+    }
+}
+
---
+++ src/com/hillel/Klasswork/lesson1/Variables.java
@@ -0,0 +1,36 @@
+package com.hillel.Klasswork.lesson1;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte myByte = 23;
+        System.out.println("byte = " + myByte);
+
+        short myShort = 345;
+        System.out.println("short = " + myShort);
+
+        int myInt = 3468;
+        System.out.println("int = " + myInt);
+
+        long myLong = 5467L;
+        System.out.println("long = " + myLong);
+
+        float myFloat = 23.456F;
+        System.out.println("float = " + myFloat);
+
+        double myDouble = 457.3456;
+        System.out.println("double = " + myDouble);
+
+        boolean myBoolean = false;
+        System.out.println("boolean = " + myBoolean);
+
+        char myChar = '\u0054';
+        System.out.println("char = " + myChar);
+
+       char myChar1 = 56756;
+        System.out.println( "char1 = " + myChar1);
+
+        String string = "Hi";
+        System.out.println("string = " + string);
+
+    }
+}
